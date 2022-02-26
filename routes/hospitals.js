efd2ad134ccd58e641a4a78be13488c362a5cb19
Teerawat Pimjoi <tpimjoi@gmail.com>
2022-02-26 14:52:47 +0700

    Authentication

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { protect, authorize } = require("../middleware/auth");
 const {
   getHospitals,
   getHospital,
   postHospital,
   putHospital,
-  deleteHospital,
+  deleteHospital
 } = require("../controllers/hospitals");
 
-router.route("/").get(getHospitals).post(postHospital);
-router.route("/:id").get(getHospital).put(putHospital).delete(deleteHospital);
+router
+  .route("/")
+  .get(getHospitals)
+  .post(protect, authorize("admin"), postHospital);
+router
+  .route("/:id")
+  .get(getHospital)
+  .put(protect, authorize("admin"), putHospital)
+  .delete(protect, authorize("admin"), deleteHospital);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 //Routes files
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
 const app = express();
 //connect to database
 connectDB();
 
 //body parser
 app.use(express.json());
+// Cookie parser
+app.use(cookieParser());
 
 //Mount routers
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
