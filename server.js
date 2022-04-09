220148c13a2fa453fbbfa1c99b974a5b0870806c
Teerawat Pimjoi <tpimjoi@gmail.com>
2022-04-09 15:18:16 +0700

    add swagger

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require("cors");
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 
 //Load env vars
@@ -20,6 +22,25 @@ const auth = require("./routes/auth");
 const app = express();
 //connect to database
 connectDB();
+
+const swaggerOptions ={
+  swaggerDefinition:{
+    openapi:'3.0.0',
+    info:{
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express VacQ API'
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1'
+      }
+    ],
+  },
+  apis:['./routes/*.js'],
+};
+const swaggerJsDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerJsDocs));
 
 //body parser
 app.use(express.json());
